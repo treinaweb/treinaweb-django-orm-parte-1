1a5fe80090071595220c57c00751c00e8608674b
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-04-02 14:52:18 -0300

    04.03_Relacionando_Entidades_Endereco_Cliente

--- clientes/models.py
+++ clientes/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 # Create your models here.
 
+class Endereco(models.Model):
+    rua = models.CharField(max_length=200, null=False, blank=False)
+    numero = models.IntegerField(null=False, blank=False)
+    complemento = models.CharField(max_length=200, null=False, blank=False)
+    bairro = models.CharField(max_length=50, null=False, blank=False)
+    cidade = models.CharField(max_length=50, null=False, blank=False)
+    pais = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.rua
+
 class Cliente(models.Model):
     SEXO_CHOICES = (
         ("F", "Feminino"),
@@ -14,17 +25,9 @@ class Cliente(models.Model):
     email = models.EmailField(null=False, blank=False)
     profissao = models.CharField(max_length=50, null=False, blank=False)
     sexo = models.CharField(max_length=1, choices=SEXO_CHOICES, blank=False, null=False)
+    endereco = models.OneToOneField(Endereco, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.nome
 
-class Endereco(models.Model):
-    rua = models.CharField(max_length=200, null=False, blank=False)
-    numero = models.IntegerField(null=False, blank=False)
-    complemento = models.CharField(max_length=200, null=False, blank=False)
-    bairro = models.CharField(max_length=50, null=False, blank=False)
-    cidade = models.CharField(max_length=50, null=False, blank=False)
-    pais = models.CharField(max_length=50, null=False, blank=False)
 
-    def __str__(self):
-        return self.rua
