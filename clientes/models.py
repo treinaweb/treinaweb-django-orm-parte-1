86dddc1be20ff092bb5311e56c54bf3c515be3f5
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-04-02 14:33:36 -0300

    04.02_Criando_Model_Endereco

--- clientes/models.py
+++ clientes/models.py
@@ -17,3 +17,14 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Endereco(models.Model):
+    rua = models.CharField(max_length=200, null=False, blank=False)
+    numero = models.IntegerField(null=False, blank=False)
+    complemento = models.CharField(max_length=200, null=False, blank=False)
+    bairro = models.CharField(max_length=50, null=False, blank=False)
+    cidade = models.CharField(max_length=50, null=False, blank=False)
+    pais = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.rua
