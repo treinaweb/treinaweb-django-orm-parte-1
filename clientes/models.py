518fb714a28a0df57e007ba23b9754701529ee94
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-04-04 14:45:21 -0300

    05.02_Criando_Model_Pedido_Relacionando_Cliente

--- clientes/models.py
+++ clientes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -12,6 +13,21 @@ class Endereco(models.Model):
 
     def __str__(self):
         return self.rua
+
+class Pedido(models.Model):
+    STATUS_CHOICES = (
+        ("P", "Pedido realizado"),
+        ("F", "Fazendo"),
+        ("E", "Saiu para entrega"),
+    )
+    cliente = models.ForeignKey("Cliente", on_delete=models.CASCADE)
+    data_pedido = models.DateTimeField(default=timezone.now)
+    valor = models.FloatField(blank=False, null=False)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, blank=False, null=False)
+    observacoes = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.cliente.nome
 
 class Cliente(models.Model):
     SEXO_CHOICES = (
