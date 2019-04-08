a1ea180f8044aecff918b60456f569270bf0e50a
fagnerpsantos <fagnerpinheirosantos@gmail.com>
2019-04-08 09:53:14 -0300

    05.07_Listando_Pedido_ID_Editando_Pedidos_Desafio

--- clientes/services/pedido_service.py
+++ clientes/services/pedido_service.py
@@ -8,3 +8,15 @@ def cadastrar_pedido(pedido):
 def listar_pedidos():
     pedidos = Pedido.objects.all()
     return pedidos
+
+def editar_pedido(pedido, pedido_novo):
+    pedido.cliente = pedido_novo.cliente
+    pedido.observacoes = pedido_novo.observacoes
+    pedido.data_pedido = pedido_novo.data_pedido
+    pedido.valor = pedido_novo.valor
+    pedido.status = pedido_novo.status
+    pedido.save(force_update=True)
+
+def listar_pedido_id(id):
+    pedido = Pedido.objects.get(id=id)
+    return pedido
--- clientes/views/pedido_views.py
+++ clientes/views/pedido_views.py
@@ -23,3 +23,22 @@ def inserir_pedido(request):
 def listar_pedidos(request):
     pedidos = pedido_service.listar_pedidos()
     return render(request, 'pedidos/lista_pedidos.html', {'pedidos': pedidos})
+
+def listar_pedido_id(request, id):
+    pedido = pedido_service.listar_pedido_id(id)
+    return render(request, 'pedidos/lista_pedido.html', {'pedido': pedido})
+
+def editar_pedido(request, id):
+    pedido_antigo = pedido_service.listar_pedido_id(id)
+    form_pedido = pedido_forms.PedidoForm(request.POST or None, instance=pedido_antigo)
+    if form_pedido.is_valid():
+        cliente = form_pedido.cleaned_data["cliente"]
+        observacoes = form_pedido.cleaned_data["observacoes"]
+        data_pedido = form_pedido.cleaned_data["data_pedido"]
+        valor = form_pedido.cleaned_data["valor"]
+        status = form_pedido.cleaned_data["status"]
+        pedido_novo = pedido.Pedido(cliente=cliente, observacoes=observacoes, data_pedido=data_pedido, valor=valor,
+                                    status=status)
+        pedido_service.editar_pedido(pedido_antigo, pedido_novo)
+        return redirect('listar_pedidos')
+    return render(request, 'pedidos/form_pedido.html', {'form_pedido': form_pedido})
